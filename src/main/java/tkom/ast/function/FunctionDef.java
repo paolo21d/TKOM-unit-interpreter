2f77aabe9a59c20ebb5ce40914caa3e58d4f0a93
paolo21d <paolo21d@gmail.com>
2020-05-24 23:17:28 +0200

    Fixed printing string

--- src/main/java/tkom/ast/function/FunctionDef.java
+++ src/main/java/tkom/ast/function/FunctionDef.java
@@ -42,11 +42,6 @@ public class FunctionDef extends Signature implements Node {
 ////////////////////////////////////////////////////////////////////
 
     public Value execute(Environment environment, List<Expression> arguments) throws RuntimeEnvironmentException {
-        if(identifier.toUpperCase().equals("PRINT")) {
-
-            return new NumberNode(0.0);
-        }
-
         checkParameters();
         setUpEnvironmentForFunction(environment, arguments);
         ExecuteOut out = block.execute(environment);
@@ -114,14 +109,6 @@ public class FunctionDef extends Signature implements Node {
         } else {
             throw new RuntimeEnvironmentException("Cannot cast return type");
         }
-    }
-
-    void executePrintFunction(Environment environment, List<Expression> arguments) throws RuntimeEnvironmentException {
-        if(arguments.size() != 1) {
-            throw new RuntimeEnvironmentException("Print function argument require 1 argument");
-        }
-
-
     }
 
 }
