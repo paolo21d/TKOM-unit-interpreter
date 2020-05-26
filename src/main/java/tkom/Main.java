eb59e861682990cf7f88ab034929208f42e7a5ca
paolo21d <paolo21d@gmail.com>
2020-05-27 00:00:05 +0200

    Final documentation

--- src/main/java/tkom/Main.java
+++ src/main/java/tkom/Main.java
@@ -1,7 +1,6 @@
 package tkom;
 
 import tkom.ast.Program;
-import tkom.data.KeyWords;
 import tkom.data.UnitRatio;
 import tkom.errorHandler.InvalidTokenException;
 import tkom.errorHandler.RuntimeEnvironmentException;
@@ -16,21 +15,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException, InvalidTokenException, UnexpectedTokenException, RuntimeEnvironmentException {
-        KeyWords.addVariableType("long");
-//        Lexer lexer = new Lexer(new FileReader("src/main/resources/example1.txt"));
-/*        Lexer lexer = new Lexer(new StringReader("function DOUBLE main() { return 0.0; }"));
-        Token token = lexer.readNextToken();
-        while (token.getType() != TokenType.EndOfFile) {
-            if (token.getType().equals(TokenType.Identifier)) {
-                System.out.println(token.getType().toString() + " " + token.getValue());
-            } else if (token.getType().equals(TokenType.NumberLiteral)) {
-                System.out.println(token.getType().toString() + " " + token.getLiteralNumber());
-            } else {
-                System.out.println(token.getType().toString());
-            }
-            token = lexer.readNextToken();
-        }*/
-
+//        KeyWords.addVariableType("long");
 //        Parser parser = new Parser(new Lexer(new StringReader("function DOUBLE main() { return 0.0; }")));
         Parser parser = new Parser(new Lexer(new FileReader("src/main/resources/example2.txt")));
         Program program = parser.parseProgram();
